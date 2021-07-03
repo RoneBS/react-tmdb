e337d3400f450448c1fecaf7e2dd3cc0ed74b5c2
Rone Silveira <ronebs10@hotmail.com>
2021-07-03 16:04:52 -0300

    updating Home with movie card

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,27 @@
+import movieImg from '../../assets/star-wars-poster.jpg'
+
+import { Header } from '../../components/Header'
 import * as S from './styes'
 
 export function Home() {
   return (
     <>
       <S.Container>
-        <S.Header>
-          <S.Title>Central Movies</S.Title>
-          <S.Search>
-            <input type="text" placeholder="procure o filme aqui" />
-          </S.Search>
-        </S.Header>
+        <Header />
 
         <S.Main>
-          <S.CardList></S.CardList>
+          <S.CardList>
+            <img src={movieImg} alt="movie photo" />
+            <div>Star Wars</div>
+          </S.CardList>
+          <S.CardList>
+            <img src={movieImg} alt="movie photo" />
+            <div>Star Wars</div>
+          </S.CardList>
+          <S.CardList>
+            <img src={movieImg} alt="movie photo" />
+            <div>Star Wars</div>
+          </S.CardList>
         </S.Main>
       </S.Container>
     </>
--- src/pages/Home/styes.ts
+++ src/pages/Home/styes.ts
@@ -1,8 +1,41 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const Container = styled.div``;
-export const Header = styled.nav``;
-export const Title = styled.h1``;
-export const Search = styled.div``;
-export const Main = styled.main``;
-export const CardList = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Main = styled.main`
+  width: 80%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 100px;
+`
+export const CardList = styled.div`
+  width: 260px;
+  height: 320px;
+  display: flex;
+  flex-direction: column;
+
+  img {
+    width: 260px;
+    height: 320px;
+  }
+
+  div {
+    width: 260px;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.5rem;
+    background: #000;
+    color: #fff;
+  }
+`
